6248282a3f9f04e26362be5a81a7d95225008f63
Teacher A <teaching.know@gmail.com>
2022-04-14 12:14:02 +0200

    turn gameplay

---
+++ turn.js
@@ -0,0 +1,22 @@
+export default class Turn {
+  constructor() {
+    this.roundNumber = 0;
+  }
+
+  startTurn = () => {
+    this.roundNumber += 1;
+    console.log(`You are now reaching round ${this.roundNumber}`);
+    if (this.roundNumber === 10) {
+      console.log("This is the last round ! Give it your all !");
+    }
+    
+    if (this.roundNumber <= 10) {
+      // faire une méthode qui concentre les actions de tours avec each turn do (si c'est vraiment avec ça lol)
+    }
+    
+  }
+
+  
+  
+  
+}
